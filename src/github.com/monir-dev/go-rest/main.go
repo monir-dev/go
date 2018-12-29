855c4dbbfb0aa965f6784343482329c4a9414b2d
Monir-dev <echomonir@gmail.com>
2018-12-29 17:15:14 +0600

    HTTP workign

--- src/github.com/monir-dev/go-rest/main.go
+++ src/github.com/monir-dev/go-rest/main.go
@@ -11,36 +11,48 @@ import (
 // 	w.Write([]byte("working..\n"))
 // }
 
+// Book Struct (Model)
+type Book struct {
+	Id     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author Struct (Model)
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Get All Books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	// init router
 	r := mux.NewRouter()
 
 	// Routes consist of a path and a handler function.
-	r.HandleFunc("/api/books", getBooks).method("GET")
-	r.HandleFunc("/api/books/{id}", getBooks).method("GET")
-	r.HandleFunc("/api/books", createBook).method("POST")
-	r.HandleFunc("/api/books/{id}", updateBook).method("PUT")
-	r.HandleFunc("/api/books", deleteBook).method("DELETE")
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBooks).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books", deleteBook).Methods("DELETE")
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":4000", r))
-}
-
-// Get All Books
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("working..\n"))
-}
-
-// Book Struct (Model)
-type Book sturct {
-	Id string `json:"id"`
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
-	Author *Author `json:"author"`
-}
-
-// Author Struct (Model)
-type Author sturct {
-	Firstname string `json:"firstname"`
-	Lastname string `json:"lastname"`
 }
