b9196f96ac2abf008ed94923bfe437daee216e3c
Monir-dev <echomonir@gmail.com>
2018-12-29 17:49:38 +0600

    working on book create

--- src/github.com/monir-dev/go-rest/main.go
+++ src/github.com/monir-dev/go-rest/main.go
@@ -1,15 +1,16 @@
 package main
 
+// https://www.youtube.com/watch?v=SonwZ6MF5BE
+
 import (
+	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
-
-// func YourHandler(w http.ResponseWriter, r *http.Request) {
-// 	w.Write([]byte("working..\n"))
-// }
 
 // Book Struct (Model)
 type Book struct {
@@ -25,13 +26,35 @@ type Author struct {
 	Lastname  string `json:"lastname"`
 }
 
+// Init books var as a slice Book struct
+var books []Book
+
 // Get All Books
 func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
 
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get params
+	// loop through bood and find with id
+	for _, item := range books {
+		if item.Id == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 func createBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.Id = strconv.Itoa(rand.Intn(10000000)) // Mock Id
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
@@ -46,9 +69,14 @@ func main() {
 	// init router
 	r := mux.NewRouter()
 
+	// Mock Data -@todo - implement DB
+	books = append(books, Book{Id: "1", Isbn: "234234", Title: "Book one", Author: &Author{Firstname: "Monir", Lastname: "Hossain"}})
+
+	books = append(books, Book{Id: "2", Isbn: "3242", Title: "Book Two", Author: &Author{Firstname: "Monir", Lastname: "Hossain"}})
+
 	// Routes consist of a path and a handler function.
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	r.HandleFunc("/api/books", createBook).Methods("POST")
 	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/api/books", deleteBook).Methods("DELETE")
